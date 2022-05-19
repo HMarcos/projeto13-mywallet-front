10285563c0f4b7ef795a0910a118a0337dd9817b
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-19 16:02:11 -0300

    Preparando revisão de código

--- src/data/links.js
+++
@@ -1,3 +0,0 @@
-const API_LINK = "https://back-end-mywallet.herokuapp.com";
-
-export default API_LINK;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-
-import App from "./components/App";
-
-const divRoot = document.querySelector(".root");
-
-
-ReactDOM.render(<App />, divRoot);
